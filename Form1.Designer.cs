019871e2ee407ecdce35d4434a08833618ba6e2c
hzawary <hashemzawary@gmail.com>
2016-07-29 21:35:38 +0430

    Add a sample code of usage fast guided filter as winform CSharp app.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WinFormDemoFastGuidedFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = Properties.Resources.color_stucki;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 361);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(516, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(480, 361);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Fast Guided Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "FGF Enhancement";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 421);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FGF Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿// Implemented test filters by HZ, March-2016
+// hashemzawary@gmail.com
+// https://www.linkedin.com/in/hashem-zavvari-53b01457
+//
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace WinFormDemoFastGuidedFilter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        readonly Stopwatch _elapsedTime = new Stopwatch();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _elapsedTime.Restart();
+
+            using (var bmp = AForge.Imaging.Image.Convert8bppTo16bpp((Bitmap)pictureBox1.Image))
+            {
+                var fastGuidedFilter = new FastGuidedFilter
+                {
+                    KernelSize = 8,
+                    Epsilon = 0.02f,
+                    SubSamplingRatio = 0.25f,
+                    OverlayImage = (Bitmap)bmp.Clone()
+                };
+
+                fastGuidedFilter.ApplyInPlace(bmp);
+                fastGuidedFilter.OverlayImage.Dispose();
+
+                pictureBox2.Image?.Dispose();
+
+                pictureBox2.Image = AForge.Imaging.Image.Convert16bppTo8bpp(bmp);
+            }
+
+            _elapsedTime.Stop();
+            Text = $"{_elapsedTime.Elapsed.Milliseconds} (ms)";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+
+            _elapsedTime.Restart();
+
+            using (var bmp = AForge.Imaging.Image.Convert8bppTo16bpp((Bitmap)pictureBox2.Image))
+            {
+                var fastGuidedFilter = new FastGuidedFilter
+                {
+                    KernelSize = 16,
+                    Epsilon = 0.16f,
+                    SubSamplingRatio = 0.25f,
+                    OverlayImage = (Bitmap)bmp.Clone()
+                };
+
+                var guidedImage = fastGuidedFilter.Apply(bmp);
+
+                var subtracted = new Subtract(guidedImage).Apply(fastGuidedFilter.OverlayImage);
+                guidedImage.Dispose();
+
+                //var mul = 100;
+                //var Multiply = FastGuidedFilter.GetFilledImage(
+                //    subtracted.Width, subtracted.Height,
+                //    subtracted.PixelFormat, Color.FromArgb(mul, mul, mul));
+
+                //new Multiply(Multiply).ApplyInPlace(subtracted);
+                //Multiply.Dispose();
+
+                new Add(subtracted).ApplyInPlace(fastGuidedFilter.OverlayImage);
+                subtracted.Dispose();
+
+                pictureBox2.Image?.Dispose();
+
+                pictureBox2.Image = AForge.Imaging.Image.Convert16bppTo8bpp(fastGuidedFilter.OverlayImage);
+
+                fastGuidedFilter.OverlayImage.Dispose();
+            }
+
+            _elapsedTime.Stop();
+            Text = $"{_elapsedTime.Elapsed.Milliseconds} (ms)";
+        }
+    }
+}
